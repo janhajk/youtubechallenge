88cd32f3e8f6b64e94a61314cb4ed1b2ca0a4d40
Jan <janhajk@gmail.com>
2016-12-09 15:45:37 +0000

    ,

--- public/app.js
+++ public/app.js
@@ -27,6 +27,11 @@
             }
             //console.log(videos);
             body.appendChild(btable(current));
+            var chart = document.createElement('canvas');
+            chart.width = '100%';
+            chart.height = '200';
+            body.appendChild(chart);
+            makeChartr(chart);
          } else {
             // Error
          }
@@ -77,7 +82,46 @@
       return tr;
    };
 
-   
+   var makeChart = function(ctx) {
+      var myChart = new Chart(ctx, {
+         type: 'bar',
+         data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+               label: '# of Votes',
+               data: [12, 19, 3, 5, 2, 3],
+               backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+               ],
+               borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+               ],
+               borderWidth: 1
+            }]
+         },
+         options: {
+            scales: {
+               yAxes: [{
+                  ticks: {
+                     beginAtZero:true
+                  }
+               }]
+            }
+         }
+      });
+   };
+
+
    /**
     * Sort object properties (only own properties will be sorted).
     * @param {object} obj object to sort properties
