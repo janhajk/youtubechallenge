f8f2064b1505b9124276be9381d0cd29c14320aa
Jan <janhajk@gmail.com>
2016-12-09 08:23:51 +0000

    .

--- app.js
+++ app.js
@@ -38,25 +38,3 @@ connection.query('SELECT * FROM videos', function(err, rows, fields) {
 connection.end();
 
 
-
-
-
-async.parallel([
-    function(callback) {
-        setTimeout(function() {
-            callback(null, 'one');
-        }, 200);
-    },
-    function(callback) {
-        setTimeout(function() {
-            callback(null, 'two');
-        }, 100);
-    }
-],
-// optional callback
-function(err, results) {
-    // the results array will equal ['one','two'] even though
-    // the second function had a shorter timeout.
-});
-
-
