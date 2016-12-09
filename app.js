de3f6991af18869b201e1a61d99e2b1645d7242e
Jan <janhajk@gmail.com>
2016-12-09 07:05:59 +0000

    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+var google = require('googleapis');
+var Youtube = Google.youtube("v3");
+ 
+var API_KEY = 'AIzaSyAQ2Hs4P-sMGzeH82QMrZYnVBGQfZl62Rw'; // specify your API key here
+google.options({ auth: API_KEY });
+Youtube.videos.list({
+   part: 'statistics',
+   id: 'JnS8qePSv90'
+}, function (err, stats){
+   console.log('Result: ' + (err ? err.message : stats));
+});
