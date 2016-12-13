52fe8959422ea3c66eeb6e92f846beb6c3c65d17
Jan <janhajk@gmail.com>
2016-12-13 10:56:58 +0000

    adapt js

--- app.js
+++ app.js
@@ -52,15 +52,8 @@ app.get('/cron', function(req, res){
    });
 });
 
-app.get('/cron2', function(req, res){
-   update.update2(connection, function(err, msg){
-      if (err) {
-         res.send(err.message);
-      }
-      else {
-         res.send(msg);
-      }
-   });
+app.get('/new', function(req, res) {
+   res.send('New Race created');
 });
 
 
--- update.js
+++ update.js
@@ -4,48 +4,7 @@ var config = require(__dirname + '/config.js');
 var async = require('async');
 
 
-var update = function(mysqlconnection, cb){
-   google.options({ auth: config.api_key });
-   mysqlconnection.query('SELECT * FROM videos', function(err, rows, fields) {
-      if(err) throw err;
-      console.log(rows);
-      var videos = [];
-      for (let i in rows) {
-         videos.push(function(callback) {
-            Youtube.videos.list({
-               part: 'statistics',
-               id: rows[i].yid
-            }, function(err, stats) {
-               console.log((err ? err.message : callback(null, {
-                  id: rows[i].id,
-                  stats: stats.items[0].statistics
-               })));
-            });
-         });
-      }
-      async.parallel(videos, function(err, results) {
-         console.log(results);
-         var queries = [];
-         for(let i in results) {
-            queries.push(function(callback) {
-               let query = 'INSERT INTO stats (vid, viewCount,timestamp) VALUES (' + results[i].id + ', ' + results[i].stats.viewCount + ', ' + Math.floor(Date.now() / 1000) + ');';
-               mysqlconnection.query(query, function(err, rows, fields) {
-                  if(err) throw err;
-                  callback(null);
-               });
-            });
-         }
-         async.parallel(queries, function(err){
-            cb('everything updated!');
-            console.log('everything updated!');
-         });
-      });
-   });
-};
-exports.update = update;
-
-
-var update2 = function(connection, callback) {
+var update = function(connection, callback) {
    var database = require(__dirname + '/database.js');
    var youtube = require(__dirname + '/youtube.js');
    database.getVideos('all', connection, function(err, rows){
@@ -66,4 +25,4 @@ var update2 = function(connection, callback) {
       }
    });
 };
-exports.update2 = update2;
+exports.update = update;
