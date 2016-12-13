b6aa279866b38e2ee4f46cbe7649023699c9918a
Jan <janhajk@gmail.com>
2016-12-13 09:06:13 +0000

    sql error

--- database.js
+++ database.js
@@ -20,7 +20,11 @@ var updateStats = function(stats, connection, callback) {
    if (config.dev) console.log(stats);
    var insert = [];
    for (let i in stats) {
-      insert.push('('+stats[i].id+','+stats[i].statistics.viewCount+','+Math.floor(Date.now() / 1000)+')');
+      let p = [];
+      p.push("'" + stats[i].id + "'");
+      p.push(stats[i].statistics.viewCount);
+      p.push(Math.floor(Date.now() / 1000));
+      insert.push('('+p.join(',')+')');
    }
    var query = 'INSERT INTO stats (yid, viewCount, timestamp) VALUES ' + insert.join(',');
    if (config.dev) console.log(query);
